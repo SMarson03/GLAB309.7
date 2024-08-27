b60abad5b46686f558abdfc439aa5683022a433f
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-08-27 10:31:58 -0400

    1

---
+++ GLAB309.7/src/main/java/com/SpringCRUDDemo/GLAB3097/Application.java
@@ -0,0 +1,13 @@
+package com.SpringCRUDDemo.GLAB3097;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
